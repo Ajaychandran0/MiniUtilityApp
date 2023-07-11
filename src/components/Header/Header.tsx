7a174b9230e5c8aa550f733af5d64de66e15b8bc
Ajay Chandran <ajaychandran2019@gmail.com>
2023-07-12 00:16:48 +0530

    fixed: responsiveness of header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,19 +4,19 @@ import './Header.css';
 
 const Header: React.FC = () => {
   return (
-    <header className="header-container">
+    <header className="header-container ">
       <nav className='navbar'>
         <Link className='link' to="/">Home</Link>
         <ul>
-        <li>
+        <li className='mr-2 sm:mr-3 text-xs  sm:text-sm md:mr-7 md:text-lg'>
             <Link to="/">Weather App</Link>
           </li>
 
-          <li>
+          <li className='mr-2 sm:mr-3 text-xs sm:text-sm md:mr-7 md:text-lg'>
             <Link to="/taskify">Taskify</Link>
           </li>
          
-          <li>
+          <li className='mr-1 text-xs sm:text-sm md:mr-7 md:text-lg'>
             <Link to="/password_generator">Password Generator</Link>
           </li>
         </ul>
